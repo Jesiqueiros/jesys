a4236bf07922be5b60c07cb75cedaa928b14758a
JesuSiqueiros <jesiqueiros@hotmail.com>
2026-09-02 13:18:40 -0700

    READY TO PRODUCTION

--- config/settings.py
+++ config/settings.py
@@ -56,11 +56,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "whitenoise.runserver_nostatic",
     "landing",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
